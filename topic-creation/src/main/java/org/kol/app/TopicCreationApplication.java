a01d5c5405504fb411a4a441700127151972d5bc
iSemicolon <palash.samanta7497@gmail.com>
2023-05-25 17:16:59 +0530

    "KAFKA-TOPIC-CREATION"

---
+++ topic-creation/src/main/java/org/kol/app/TopicCreationApplication.java
@@ -0,0 +1,13 @@
+package org.kol.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicCreationApplication.class, args);
+	}
+
+}
