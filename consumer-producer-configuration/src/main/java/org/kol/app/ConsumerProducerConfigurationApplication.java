2dad55d1d8c8726819940afcf17b166e5e96d672
iSemicolon <palash.samanta7497@gmail.com>
2023-05-25 17:06:30 +0530

    "PRODUCER-CONSUMER-CONFIGURATION"

---
+++ consumer-producer-configuration/src/main/java/org/kol/app/ConsumerProducerConfigurationApplication.java
@@ -0,0 +1,13 @@
+package org.kol.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerProducerConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerProducerConfigurationApplication.class, args);
+	}
+
+}
