9e0a3cf757fae902c525d774212fb9bacfa28cc0
iSemicolon <palash.samanta7497@gmail.com>
2023-07-15 21:41:30 +0530

    "KAFKA CONSUMER"

---
+++ create-consumer/src/main/java/org/kol/app/CreateConsumerApplication.java
@@ -0,0 +1,22 @@
+package org.kol.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateConsumerApplication.class, args);
+	}
+
+
+	/*
+
+	1. consumer-producer configuration
+	2. TOPIC CONFIGURATION
+	3. PRODUCER CREATION
+	4. REST API CREATION
+	5. CONSUMER CREATION
+	 */
+}
